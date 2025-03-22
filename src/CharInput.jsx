98e39531996c3a7f2a1457751f5530e7b77dee74
anwolosz <andras.wolosz@nng.com>
2025-03-22 20:57:04 +0100

    Proper focus handling

--- src/CharInput.jsx
+++ src/CharInput.jsx
@@ -1,7 +1,7 @@
 import {React, useState} from 'react';
 import { specialCharacters, validLetters } from './utils/characters';
 
-function CharInput({char, onChange, valid}) {
+function CharInput({char, onChange, valid, id}) {
     const [inputValue, setInputValue] = useState("");
 
   return (
@@ -15,13 +15,15 @@ function CharInput({char, onChange, valid}) {
             const inputSymbol = e.key.toUpperCase()
             if (validLetters.includes(inputSymbol)) { // Only allow printable characters
                 setInputValue(inputSymbol);
-                onChange(inputSymbol);
+                onChange(inputSymbol, id);
             }
             e.preventDefault(); // Prevent default input behavior
         }}
+        onChange={(e) => e}
         value={specialCharacters.includes(char) ? char : inputValue}
         readOnly={valid || specialCharacters.includes(char)}
         disabled={valid || specialCharacters.includes(char)}
+        id={id}
         />
   );
 }
--- src/ProverbInput.jsx
+++ src/ProverbInput.jsx
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import CharInput from './CharInput';
 import { validLetters } from './utils/characters';
 
@@ -6,12 +6,59 @@ function ProverbInput({proverb, onProverbChange, onWin}) {
     const [inputs, setInputs] = useState(proverb.split("").map(() => ""));
     const [valids, setValids] = useState(proverb.split("").map((char) => !validLetters.includes(char) ? true : false ));
 
-    const handleInputChange = (index, value) => {
+    const handleInputChange = (index, value, id) => {
         const updatedInputs = [...inputs];
         updatedInputs[index] = value.toUpperCase();
         setInputs(updatedInputs);
+        focusNextInput(id)
+        console.log(id)
         console.log(inputs)
+        console.log(index, value, id)
     };
+
+    const focusNextInput = (id) => {
+        focusInput(id+1)
+    }
+
+    const focusPreviousInput = (id) => {
+        focusInput(id-1)
+    }
+
+    const focusInput = (id) => {
+        requestAnimationFrame(() => {
+            const element = document.getElementById((id).toString());
+            if (element)
+            {
+                element.focus();
+            }
+        });
+    }
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+          const id = parseInt(document.activeElement.id);
+      
+          switch (event.key) {
+            case "ArrowRight":
+            case " ":
+              focusNextInput(id);
+              break;
+            case "ArrowLeft":
+            case "Backspace":
+              focusPreviousInput(id);
+              break;
+            default:
+              return;
+          }
+        };
+      
+        document.addEventListener("keydown", handleKeyDown);
+      
+        return () => {
+          document.removeEventListener("keydown", handleKeyDown);
+        };
+      }, []);
+      
 
     const validateInput = () => {
         const updatedValids = [...valids];
@@ -38,6 +85,7 @@ function ProverbInput({proverb, onProverbChange, onWin}) {
     }
 
 
+    let idCounter = 0;
   return (
     <>
     <p>{JSON.stringify(valids)}</p>
@@ -47,12 +95,16 @@ function ProverbInput({proverb, onProverbChange, onWin}) {
             if (item === ' ') {
                 return <div key={index} style={{ width: '30px' }} />;
                 } else {
-                    return <CharInput
-                        validChar={item}
-                        key={index}
-                        onChange={(value) => handleInputChange(index, value)}
-                        valid={valids[index]}
-                        char={item}/>
+                    return (
+                        <CharInput
+                            id={validLetters.includes(item) ? idCounter++ : null}
+                            validChar={item}
+                            key={index}
+                            onChange={(value, id) => handleInputChange(index, value, id)}
+                            valid={valids[index]}
+                            char={item}
+                        />
+                    );
                 }
             })}
         </div>
